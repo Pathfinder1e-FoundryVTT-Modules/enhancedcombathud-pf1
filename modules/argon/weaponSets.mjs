d43126b20f499092ecc96bd9e1538e81009107a1
KitCat <katsulynx@gmail.com>
2024-04-11 18:06:43 +0100

    Add default weapon sets for actors Closes #13

--- modules/argon/weaponSets.mjs
+++ modules/argon/weaponSets.mjs
@@ -30,5 +30,34 @@ export function weaponSets(ARGON) {
 
             await this.actor.updateEmbeddedDocuments("Item", updates);
         }
+
+        async _getSets() {
+            const sets = mergeObject(await this.getDefaultSets(), deepClone(this.actor.getFlag("enhancedcombathud", "weaponSets") || {}));
+
+            for (const [set, slots] of Object.entries(sets)) {
+                slots.primary = slots.primary ? await this.actor.items.get(slots.primary) : null;
+                slots.secondary = slots.secondary ? await this.actor.items.get(slots.secondary) : null;
+            }
+            return sets;
+        }
+
+        async getDefaultSets() {
+            const attacks = this.actor.itemTypes.attack;
+
+            return {
+                1: {
+                    primary: attacks[0]?.id,
+                    secondary: attacks[1]?.id
+                },
+                2: {
+                    primary: attacks[2]?.id,
+                    secondary: attacks[3]?.id
+                },
+                3: {
+                    primary: attacks[4]?.id,
+                    secondary: attacks[5]?.id
+                },
+            }
+        }
     }
 }
