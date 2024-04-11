fc904d96b586dcd02ddf10264000325de9ef93a5
KitCat <katsulynx@gmail.com>
2024-04-11 19:32:33 +0100

    Fix Item Drag & Drop to weapon sets

--- modules/argon/weaponSets.mjs
+++ modules/argon/weaponSets.mjs
@@ -31,12 +31,32 @@ export function weaponSets(ARGON) {
             await this.actor.updateEmbeddedDocuments("Item", updates);
         }
 
+        async _onDrop(event) {
+            try {
+                event.preventDefault();
+                event.stopPropagation();
+                const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+                if(data?.type !== "Item") return;
+                const set = event.currentTarget.dataset.set;
+                const slot = event.currentTarget.dataset.slot;
+                const sets = this.actor.getFlag("enhancedcombathud", "weaponSets") || {};
+
+                sets[set] = sets[set] || {};
+                sets[set][slot] = data.uuid.split(".").pop();
+
+                await this.actor.setFlag("enhancedcombathud", "weaponSets", sets);
+                await this.render();
+            } catch (error) {
+
+            }
+        }
+
         async _getSets() {
             const sets = mergeObject(await this.getDefaultSets(), deepClone(this.actor.getFlag("enhancedcombathud", "weaponSets") || {}));
 
             for (const [set, slots] of Object.entries(sets)) {
-                slots.primary = slots.primary ? await this.actor.items.get(slots.primary) : null;
-                slots.secondary = slots.secondary ? await this.actor.items.get(slots.secondary) : null;
+                slots.primary = typeof slots.primary === "string" ? await this.actor.items.get(slots.primary) : slots.primary;
+                slots.secondary = typeof slots.secondary === "string" ? await this.actor.items.get(slots.secondary) : slots.secondary;
             }
             return sets;
         }
@@ -46,16 +66,16 @@ export function weaponSets(ARGON) {
 
             return {
                 1: {
-                    primary: attacks[0]?.id,
-                    secondary: attacks[1]?.id
+                    primary: attacks[0] || null,
+                    secondary: attacks[1] || null
                 },
                 2: {
-                    primary: attacks[2]?.id,
-                    secondary: attacks[3]?.id
+                    primary: attacks[2] || null,
+                    secondary: attacks[3] || null
                 },
                 3: {
-                    primary: attacks[4]?.id,
-                    secondary: attacks[5]?.id
+                    primary: attacks[4] || null,
+                    secondary: attacks[5] || null
                 },
             }
         }
